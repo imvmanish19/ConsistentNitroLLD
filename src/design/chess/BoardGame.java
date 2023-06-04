384e88b4e8ec5b987a31f8ca69f79f398c0195c9
Manish V <imvmanish19@gmail.com>
2023-06-04 19:25:44 +0530

    Awesome use of abstract class

--- src/design/chess/BoardGame.java
+++ src/design/chess/BoardGame.java
@@ -2,7 +2,7 @@ package design.chess;
 
 import java.util.Queue;
 
-public class BoardGame {
+public abstract class BoardGame {
     private final Board board;
     private final Queue<Player> players;
 
@@ -19,6 +19,15 @@ public class BoardGame {
             Move move = currentPlayer.makeMove();
             /** Apply the move on the board **/
             board.applyMove(move);
+            /** Current player will win if his move made him win right! **/
+            if(isOver()) {
+                System.out.println("Game Over: " + currentPlayer.getName() + " has won!");
+                break;
+            }
+            /** Add player back to queue **/
+            players.add(currentPlayer);
         }
     }
+
+    public abstract boolean isOver();
 }
